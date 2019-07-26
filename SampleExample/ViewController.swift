22f87c350c8f475175495be6ffed00c7c480bd8c
Girish k <girimarbh@gmail.com>
2019-07-26 10:54:35 +0530

    Initial Commit

---
+++ SampleExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleExample
+//
+//  Created by imgadmin on 26/07/19.
+//  Copyright © 2019 Wipro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
